246b7594fc5cbdd9abe49c63c0fdeddb23146948
scolpitts536 <81165940+scolpitts536@users.noreply.github.com>
2022-10-16 14:30:45 -0500

    PLS?

--- actionList.js
+++ actionList.js
@@ -1282,7 +1282,7 @@ Action.SmallDungeon = new DungeonAction("Small Dungeon", 0, {
     finish() {
         handleSkillExp(this.skills);
 		}
-    },
+    
     story(completed)) {
         unlockStory("smallDungeonAttempted");
         if (towns[this.townNum][this.varName + "LoopCounter"] >= 42) unlockStory("clearSDungeon");
